60e6589cd3bf57900b38872a0259d1b610f0aba1
meera-digi <meera.gowdu@digidense.in>
2025-06-06 12:06:16 +0530

    Update test_blog.py

--- Fast_API/tests/test_blog.py
+++ Fast_API/tests/test_blog.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 
-import pytest
+import testfolder
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 from api.router.blog import router
