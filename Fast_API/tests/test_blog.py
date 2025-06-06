2f9e850d8ddf1876151862ff1d70deba24c9a728
AnishaDevops <anishacsc2001@gmail.com>
2025-06-06 10:25:48 +0530

    update router files

--- Fast_API/tests/test_blog.py
+++ Fast_API/tests/test_blog.py
@@ -4,8 +4,6 @@ import os
 # Add parent dir (Fast_API) to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-
 import pytest
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
