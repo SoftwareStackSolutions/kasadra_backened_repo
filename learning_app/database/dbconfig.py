a422f298e4120ed608fbd87b80aca3ae93a825e4
akhilesh-mony <akhilesh.mony@digidense.in>
2025-08-07 15:18:48 +0530

    change db config.py file

--- learning_app/database/dbconfig.py
+++ learning_app/database/dbconfig.py
@@ -15,14 +15,14 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Loads .env if present
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("POSTGRES_DB")
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
 )
 
 
