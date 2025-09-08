f2a472fb8bcd748d55d4b1908bfc8a8444549be7
akhilesh-mony <akhilesh.mony@digidense.in>
2025-09-08 11:32:15 +0530

    Add date and time colume

--- learning_app/database/dbconfig.py
+++ learning_app/database/dbconfig.py
@@ -22,7 +22,7 @@ load_dotenv()  # Loads .env if present
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT") or 5432
 DB_NAME = os.getenv("POSTGRES_DB")
 
 SQLALCHEMY_DATABASE_URL = (
