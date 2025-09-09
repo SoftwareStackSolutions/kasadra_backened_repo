e9451a1a941400fd87279a4f17e3403dce765dc5
akhilesh-mony <akhilesh.mony@digidense.in>
2025-09-09 23:12:10 +0530

    Devloped Add Course API

--- learning_app/models/course.py
+++ learning_app/models/course.py
@@ -1,24 +1,33 @@
-# from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import relationship
-# from .base import Base
-# from .user import User
-
-# class Course(Base):
-#     __tablename__ = "courses"
-
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String, nullable=False)
-#     description = Column(String, nullable=False)
-#     duration = Column(String, nullable=False)
-#     instructor_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-#     thumbnail = Column(String, nullable=True)  # store thumbnail URL/path
-
-#     instructor = relationship("User", back_populates="courses")
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from .base import Base
+from datetime import date
+from models.user import User
 
 
-# class User(Base):
-#     __tablename__ = "users"
-    
-#     # existing fields...
-#     token = relationship("Token", back_populates="user", uselist=False)
-#     courses = relationship("Course", back_populates="instructor")  # add this
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    duration = Column(String, nullable=False)
+    thumbnail = Column(String, nullable=True)
+    instructor_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(Date, default=date.today)
+    #instructor = relationship(User, back_populates="courses")
+
+
+
+# class Token(Base):
+#     __tablename__ = "tokens"
+#     __table_args__ = {'extend_existing': True}
+
+#     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+#     access_token = Column(String(450), unique=True)
+#     refresh_token = Column(String(450), nullable=False)
+#     status = Column(Boolean)
+#     created_at = Column(Date, default=date.today)
+#     user = relationship(User, back_populates="token")
+
+
