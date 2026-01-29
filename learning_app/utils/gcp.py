232734f574a3e0919dd25b03f1ff9d7892f03049
UT-B-Ajai <ajai.m@theunitytech.com>
2026-01-29 16:48:16 +0530

    upate gcp file

--- learning_app/utils/gcp.py
+++ learning_app/utils/gcp.py
@@ -6,11 +6,15 @@ async def upload_file_to_gcs(file, folder_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
 
-    # Generate unique filename
     unique_name = f"{folder_name}/{uuid.uuid4()}_{file.filename}"
-
     blob = bucket.blob(unique_name)
-    blob.upload_from_file(file.file, content_type=file.content_type)
+
+    # 🔥 FIX STARTS HERE
+    blob.content_type = file.content_type or "application/octet-stream"
+    blob.content_disposition = "inline"
+    # 🔥 FIX ENDS HERE
+
+    blob.upload_from_file(file.file)
 
     public_url = f"https://storage.googleapis.com/{bucket_name}/{unique_name}"
     return public_url
