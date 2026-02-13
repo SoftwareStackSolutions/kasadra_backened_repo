bc6179672f2324c3e3f4250f103d3515efc8fb13
Akhileshml <akhilesh.mony@digidense.in>
2026-02-13 10:14:39 +0530

    remove line

--- learning_app/models/tenent/email_otp.py
+++ learning_app/models/tenent/email_otp.py
@@ -13,6 +13,3 @@ class EmailOTP(Base):
     resend_count = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.utcnow)
     # created_at = Column(DateTime(timezone=True), server_default=func.now())
-
-
-
