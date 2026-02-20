1adeec0b4d6231c60cb55d252585bc31cbc74b8c
anishadevopz <anisha.william@digidense.in>
2026-02-20 11:56:06 +0530

    update jwt auth files

--- learning_app/routes/tenent/auth.py
+++ learning_app/routes/tenent/auth.py
@@ -6,6 +6,7 @@ router = APIRouter(prefix="/tenant", tags=["Tenant Auth"])
 
 @router.get("/me")
 async def get_me(current_org: dict = Depends(get_current_org)):
+    
     return {
         "org_id": current_org.get("org_id"),
         "email": current_org.get("email"),
