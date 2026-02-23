f0bcb4f3549e72f63230554913396786a28728ad
anishadevopz <anisha.william@digidense.in>
2026-02-23 13:49:19 +0530

    new updated

--- learning_app/routes/tenent/auth.py
+++ learning_app/routes/tenent/auth.py
@@ -1,9 +1,9 @@
-# from fastapi import APIRouter, Depends
-# from core.security import get_current_org
+from fastapi import APIRouter, Depends
+from core.security import get_current_org
 
-# router = APIRouter(tags=["Tenant Auth"])
+router = APIRouter(tags=["Tenant Auth"])
 
-# @router.get("/me")
+@router.get("/me")
 
-# async def get_current_org(current_org=Depends(get_current_org)):
-#     return current_org
+async def get_current_org(current_org=Depends(get_current_org)):
+    return current_org
